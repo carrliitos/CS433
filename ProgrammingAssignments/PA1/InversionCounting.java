0c320983aff2eda7843c27d14b5823e363bada69
carrliitos <salazar.ten80@gmail.com>
2020-09-23 23:34:31 -0500

    finished -- could not implement mergesort on selection

--- ProgrammingAssignments/PA1/InversionCounting.java
+++ ProgrammingAssignments/PA1/InversionCounting.java
@@ -23,12 +23,73 @@ public class InversionCounting {
 		return countInversions(0, n - 1);
 	}
 
+	// private void mergesort(int left, int right) {
+	// 	if(left == right) {
+	// 		return 0;
+	// 	}
+
+	// 	int mid = (left + ((right - left) >> 1));
+	// 	int inversionCount = 0;
+
+	// 	inversionCount += mergesort(left, mid);
+	// 	inversionCount += mergesort(mid + 1, right);
+	// 	inversionCount += countInversions(left, right);
+
+	// 	return inversionCount;
+	// }
+
 	private int countInversions(int left, int right) { // complete this function
+        /* Brute-force algo */
         int count = 0;
         for (int i = 0; i <= n - 2; i++)
             for (int j = i + 1; j <= n - 1; j++)
                 if (array[i] > array[j])
                     count++;
         return count;
+
+        /* Failed Merge-sort approach*/
+		// int count = 0;
+		// int mid = (left + right) / 2;
+		// int i = left, j = mid + 1, k = left;
+		// if(array.length == 1) {
+		// 	return 0;
+		// }
+		// while(left <= i && i <= j && j <= mid && array[i] >= array[j]) {
+		// 	countInversions(left, mid);
+		// 	mergedArray[k++] = array[i++];
+		// 	count++;
+		// }
+		// while(mid <= i && i <= j && j <= right && array[i] >= array[j]) {
+		// 	countInversions(mid + 1, right);
+		// 	mergedArray[k++] = array[j++];
+		// 	count++;
+		// }
+		// i = left;
+		// while(i <= mid && j >= mid && array[i] >= array[j]) {
+		// 	array[i] = mergedArray[i];
+		// 	i++;
+		// 	count++;
+		// }
+		// return count;
+
+        // int k = left, i = left, j = mid + 1;
+        // int inversionCount = 0;
+        // while(i <= mid && j <= right) {
+        // 	if(array[i] <= array[j]) {
+        // 		mergedArray[k++] = array[i++];
+        // 	}else {
+        // 		mergedArray[k++] = array[j++];
+        // 		inversionCount += (mid - i + 1);
+        // 	}
+        // }
+
+        // while(i <= mid) {
+        // 	mergedArray[k++] = array[i++];
+        // }
+
+        // for(i = left; i <= right; i++) {
+        // 	array[i] = mergedArray[i];
+        // }
+        // return inversionCount;
 	}
 }
