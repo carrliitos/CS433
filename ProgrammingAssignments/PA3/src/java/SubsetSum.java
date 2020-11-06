1ac471a5ad9c61d03a98f0dae918a145d2d68994
carrliitos <salazar.ten80@gmail.com>
2020-11-06 11:11:02 -0600

    finished SubsetSum.java

--- ProgrammingAssignments/PA3/src/java/SubsetSum.java
+++ ProgrammingAssignments/PA3/src/java/SubsetSum.java
@@ -1,10 +1,24 @@
-// @author Abraham
-
 import java.util.Iterator;
 import java.util.TreeSet;
 
 public class SubsetSum {
-
-	public static boolean isSumPossible(final int elements[], int numElements, int target) { // complete this function
+	public static boolean isSumPossible(final int elements[], int numElements, int target) {
+		TreeSet<Integer> sums = new TreeSet<Integer>();
+		sums.add(0);
+		for(int i = 0; i < numElements; i++) {
+			int[] values = new int[sums.size()];
+			Iterator<Integer> it = sums.iterator();
+			int count = 0;
+			while(it.hasNext()) {
+				values[count] = it.next();
+				count++;
+			}
+			for(int j = 0; j < values.length; j++) {
+				int val = elements[i] + values[j];
+				if(val == target) return true;
+				else if(val < target) sums.add(val);
+			}
+		}
+		return false;
 	}
 }
