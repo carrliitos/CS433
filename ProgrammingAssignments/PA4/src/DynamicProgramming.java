7d1cd588387f3e560fe0eed896012fa3d80752e6
carrliitos <salazar.ten80@gmail.com>
2020-11-16 20:38:27 -0600

    attempted LIS on DynamicProgramming

--- ProgrammingAssignments/PA4/src/DynamicProgramming.java
+++ ProgrammingAssignments/PA4/src/DynamicProgramming.java
@@ -5,7 +5,22 @@ public class DynamicProgramming {
 	public static int numberOfBinaryStringsWithNoConsecutiveOnes(int n) { // complete this method
 	}
 
-	public static ArrayList<Integer> longestIncreasingSubsequence(int[] arr, int len) { // complete this method
+	public static ArrayList<Integer> longestIncreasingSubsequence(int[] arr, int len) {
+		int lis[] = new int[len];
+		int pred[] = new int[len];
+		for(int i = 0; i < len; i++) {
+			lis[i] = 1;
+			// pred[i] = -1;
+			pred[i] = i;
+		}
+
+		/*
+		* Among the indexes 0, 1, 2, . . . , i − 1, find the index maxIndex such 
+		* that arr[maxIndex] < arr[i] and LIS[maxIndex] is the maximum of all 
+		* the values among LIS[0], LIS[1], . . . , LIS[i − 1]. 
+		* If the values arr[0], arr[1], . . . , arr[i − 1] are all greater 
+		* than arr[i], then let maxIndex = −1
+		*/
 	}
 
 	private static void reverse(ArrayList<Integer> list) {
