e6a2c8b613ee8845eb6d4d8add0e9d1b2cae9284
carrliitos <salazar.ten80@gmail.com>
2020-11-16 20:11:05 -0600

    finished Johnson and Dijkstra

--- ProgrammingAssignments/PA4/src/Dijkstra.java
+++ ProgrammingAssignments/PA4/src/Dijkstra.java
@@ -1,9 +1,43 @@
+import java.util.Arrays;
+
 public class Dijkstra extends Graph {
 
 	public Dijkstra(Graph graph) {
 		super(graph);
 	}
 
-	public int[] execute(int source) { // complete this method
+	public int[] execute(int source) {
+		int distance[] = new int[numVertices];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+
+		boolean closed[] = new boolean[numVertices];
+		Arrays.fill(closed, false);
+
+		PriorityQueue<Integer> open = new PriorityQueue<Integer>();
+		open.setPriority(source, 0);
+		distance[source] = 0;
+		while(open.getSize() > 0) {
+			/* 
+			* Let minVertex be an open vertex with the minimum distance value. 
+			* Use the getMinimumItem() of open to get this vertex. 
+			* Delete the minimum in open by calling deleteMinimum().
+			*/
+			int minVertex = open.getMinimumItem();
+			open.deleteMinimum();
+			closed[minVertex] = true;
+
+			for(Edge edge : super.adjList.get(minVertex)) {
+				int adjVertex = edge.dest;
+				if(!closed[adjVertex]) {
+					int newDist = distance[minVertex] + edge.weight;
+					if(newDist < distance[adjVertex]) {
+						distance[adjVertex] = newDist;
+						open.setPriority(adjVertex, newDist);
+					}
+				}
+			}
+		}
+
+		return distance;
 	}
 }
--- ProgrammingAssignments/PA4/src/Johnson.java
+++ ProgrammingAssignments/PA4/src/Johnson.java
@@ -6,6 +6,47 @@ public class Johnson extends Graph {
 		super(graph);
 	}
 
-	public int[][] execute() throws Exception { // complete this method
+	public int[][] execute() throws Exception {
+		adjList.add(new ArrayList<Edge>());
+		for(int i = 0; i < numVertices; i++) {
+			Edge e = new Edge(numVertices, i, 0);
+			adjList.get(numVertices).add(e);
+		}
+
+		numEdges += numVertices;
+		numVertices++;
+		BellmanFord bellmanFord = new BellmanFord(this);
+		int phi[] = bellmanFord.execute(numVertices - 1);
+		adjList.remove(numVertices - 1);
+		numVertices--;
+		numEdges -= numVertices;
+		if(phi == null) { return null; }
+		for(int i = 0; i < numVertices; i++) {
+			for(int j = 0; j < adjList.get(i).size(); j++) {
+				Edge e = adjList.get(i).get(j);
+				e.weight = e.weight + phi[e.src] - phi[e.dest];
+			}
+		}
+
+		int allPairMatrix[][] = new int[numVertices][];
+		Dijkstra dijsktra = new Dijkstra(this);
+		for(int i = 0; i < numVertices; i++) { allPairMatrix[i] = dijsktra.execute(i); }
+
+		for(int i = 0; i < numVertices; i++) {
+			for(int j = 0; j < numVertices; j++) {
+				if(i != j && allPairMatrix[i][j] != Integer.MAX_VALUE) {
+					allPairMatrix[i][j] = allPairMatrix[i][j] - phi[i] + phi[j];
+				}
+			}
+		}
+
+		for(int i = 0; i < numVertices; i++) {
+			for(int j = 0; j < adjList.get(i).size(); j++) {
+				Edge e = adjList.get(i).get(j);
+				e.weight = e.weight - phi[e.src] + phi[e.dest];
+			}
+		}
+		
+		return allPairMatrix;	
 	}
 }
