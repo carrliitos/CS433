1f1b5e2b49c420342d0858f394bc06b3da33c877
carrliitos <salazar.ten80@gmail.com>
2020-11-17 22:43:39 -0600

    added PA4Report.pdf

--- ProgrammingAssignments/PA4/src/DynamicProgramming.java
+++ ProgrammingAssignments/PA4/src/DynamicProgramming.java
@@ -19,39 +19,6 @@ public class DynamicProgramming {
 	public static ArrayList<Integer> longestIncreasingSubsequence(int[] arr, int len) {
 		int lis[] = new int[len];
 		int pred[] = new int[len];
-				
-		// for(int i = 0; i < len; i++) {
-		// 	lis[i] = 1;
-		// 	pred[i] = -1;
-		// 	for(int j = 0; j < i; j++) {
-		// 		if(arr[i] > arr[maxIndex] && lis[maxIndex] > lis[i]) { 
-		// 			maxIndex = i;
-		// 		}
-		// 		if(arr[i + 1] > arr[i]) { maxIndex = -1; }
-		// 		if(maxIndex != -1) {
-		// 			lis[i] = lis[maxIndex] + 1;
-		// 			pred[i] = maxIndex;
-		// 		}
-		// 	}
-		// }
-
-		// int lisIndex = 0;
-		// for(int i = 0; i < len; i++) {
-		// 	if(lis[i] > lis[maxIndex]) { 
-		// 		maxIndex = i; 
-		// 		lisIndex = maxIndex;
-		// 	}
-		// }
-
-		// ArrayList<Integer> dynamicArray = new ArrayList<>();
-
-		// dynamicArray.add(arr[lisIndex]);
-		// int temp = pred[lisIndex];
-		// while(lisIndex != temp) {
-		// 	lisIndex = temp;
-		// 	dynamicArray.add(arr[lisIndex]);
-		// 	temp = pred[lisIndex];
-		// }
 
 		/*
 		* Among the indexes 0, 1, 2, . . . , i − 1, find the index maxIndex such 
