45110130125ad069037cfcfe1c498ab309159fab
carrliitos <salazar.ten80@gmail.com>
2020-12-11 11:10:07 -0600

    finished KMP

--- ProgrammingAssignments/PA5/src/KMP.java
+++ ProgrammingAssignments/PA5/src/KMP.java
@@ -1,10 +1,57 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class KMP {
 
-	public static ArrayList<Integer> runKMP(final String text, final String pattern) { // complete this method
+	public static ArrayList<Integer> runKMP(final String text, final String pattern) {
+		ArrayList<Integer> occ = new ArrayList<Integer>();
+		int F[] = computeFailure(pattern);
+		int txtLen = text.length();
+		int patLen = pattern.length();
+		int t = 0, p = 0;
+		
+		while(t < txtLen) {
+			if(pattern.charAt(p) == text.charAt(t)) {
+				if(p == patLen - 1) {
+					occ.add(t - p);
+					p = F[p];
+				}else {
+					p++;
+				}
+				
+				t++;
+			}else {
+				if(p != 0) {
+					p = F[p - 1];
+				}else {
+					t++;
+				}
+			}
+		}
+
+		return occ;
 	}
 
-	private static int[] computeFailure(final String pattern) { // complete this method
+	private static int[] computeFailure(final String pattern) {
+		int patLen = pattern.length();
+		int F[] = new int[patLen];
+		int pref = 0;
+		int suff = 1;
+		F[0] = 0;
+
+		while(suff != patLen) {
+			if(pattern.charAt(suff) == pattern.charAt(pref)) {
+				pref++;
+				F[suff] = pref;
+				suff++;
+			}else if(pref == 0) {
+				F[suff] = 0;
+				suff++;
+			}else {
+				pref = F[pref - 1];
+			}
+		}
+
+		return F;
 	}
 }
--- ProgrammingAssignments/PA5/src/TestCorrectness.java
+++ ProgrammingAssignments/PA5/src/TestCorrectness.java
@@ -3,52 +3,52 @@ import java.util.ArrayList;
 
 public class TestCorrectness {
 
-	static final String MST_GRAPH_PATH = "mst_graph.txt";
-	static final String SCC1_GRAPH_PATH = "scc1.txt";
-	static final String SCC2_GRAPH_PATH = "scc2.txt";
-	static final String SCC3_GRAPH_PATH = "scc3.txt";
-	static final String SCC4_GRAPH_PATH = "scc4.txt";
+	static final String MST_GRAPH_PATH = "TextFiles/mst_graph.txt";
+	static final String SCC1_GRAPH_PATH = "TextFiles/scc1.txt";
+	static final String SCC2_GRAPH_PATH = "TextFiles/scc2.txt";
+	static final String SCC3_GRAPH_PATH = "TextFiles/scc3.txt";
+	static final String SCC4_GRAPH_PATH = "TextFiles/scc4.txt";
 
-	private static void testUnionFind() {
-		UnionFind uf = new UnionFind(16);
-		System.out.println("Initial sets are 0-15\n");
-		for (int i = 0; i < 15; i += 4) {
-			System.out.printf("UNION(%d,%d)%n", i, i + 1);
-			uf.doUnion(i, i + 1);
-		}
-		System.out.println();
-		for (int i = 0; i < 16; i++) {
-			System.out.printf("List containing %2d: ", i);
-			uf.find(i).printList();
-		}
-		System.out.println("\nUNION(0,5)");
-		System.out.println("UNION(10,12)");
-		System.out.println("UNION(0,10)\n");
-		uf.doUnion(0, 5);
-		uf.doUnion(10, 12);
-		uf.doUnion(0, 10);
-		for (int i = 0; i < 16; i++) {
-			System.out.printf("List containing %2d: ", i);
-			uf.find(i).printList();
-		}
-		System.out.println("\nUNION(6,8)");
-		System.out.println("UNION(8,5)\n");
-		uf.doUnion(6, 8);
-		uf.doUnion(8, 5);
-		for (int i = 0; i < 16; i++) {
-			System.out.printf("List containing %2d: ", i);
-			uf.find(i).printList();
-		}
-	}
+	// private static void testUnionFind() {
+	// 	UnionFind uf = new UnionFind(16);
+	// 	System.out.println("Initial sets are 0-15\n");
+	// 	for (int i = 0; i < 15; i += 4) {
+	// 		System.out.printf("UNION(%d,%d)%n", i, i + 1);
+	// 		uf.doUnion(i, i + 1);
+	// 	}
+	// 	System.out.println();
+	// 	for (int i = 0; i < 16; i++) {
+	// 		System.out.printf("List containing %2d: ", i);
+	// 		uf.find(i).printList();
+	// 	}
+	// 	System.out.println("\nUNION(0,5)");
+	// 	System.out.println("UNION(10,12)");
+	// 	System.out.println("UNION(0,10)\n");
+	// 	uf.doUnion(0, 5);
+	// 	uf.doUnion(10, 12);
+	// 	uf.doUnion(0, 10);
+	// 	for (int i = 0; i < 16; i++) {
+	// 		System.out.printf("List containing %2d: ", i);
+	// 		uf.find(i).printList();
+	// 	}
+	// 	System.out.println("\nUNION(6,8)");
+	// 	System.out.println("UNION(8,5)\n");
+	// 	uf.doUnion(6, 8);
+	// 	uf.doUnion(8, 5);
+	// 	for (int i = 0; i < 16; i++) {
+	// 		System.out.printf("List containing %2d: ", i);
+	// 		uf.find(i).printList();
+	// 	}
+	// }
 
-	private static void testKruskal() throws FileNotFoundException {
-		Kruskal kruskal = new Kruskal(MST_GRAPH_PATH, GraphType.WEIGHTED);
-		ArrayList<Edge> mst = kruskal.runKruskal();
-		int mstWeight = 0;
-		for (Edge e : mst)
-			mstWeight += e.weight;
-		System.out.printf("MST has weight %d%nThe edges are: %s%n", mstWeight, mst);
-	}
+	// private static void testKruskal() throws FileNotFoundException {
+	// 	Kruskal kruskal = new Kruskal(MST_GRAPH_PATH, GraphType.WEIGHTED);
+	// 	ArrayList<Edge> mst = kruskal.runKruskal();
+	// 	int mstWeight = 0;
+	// 	for (Edge e : mst)
+	// 		mstWeight += e.weight;
+	// 	System.out.printf("MST has weight %d%nThe edges are: %s%n", mstWeight, mst);
+	// }
 
 	private static void testKMPHelper(String text, String patterns[]) {
 		System.out.println("*** Text is " + text + " *** \n");
@@ -71,33 +71,33 @@ public class TestCorrectness {
 		testKMPHelper(text, patterns);
 	}
 
-	private static void testSCC() throws FileNotFoundException {
-		SCC scc = new SCC(SCC1_GRAPH_PATH, GraphType.UNWEIGHTED);
-		ArrayList<ArrayList<Integer>> components = scc.execute();
-		System.out.printf("Components of Graph 1 are: %s%n", components);
+	// private static void testSCC() throws FileNotFoundException {
+	// 	SCC scc = new SCC(SCC1_GRAPH_PATH, GraphType.UNWEIGHTED);
+	// 	ArrayList<ArrayList<Integer>> components = scc.execute();
+	// 	System.out.printf("Components of Graph 1 are: %s%n", components);
 
-		scc = new SCC(SCC2_GRAPH_PATH, GraphType.UNWEIGHTED);
-		components = scc.execute();
-		System.out.printf("Components of Graph 2 are: %s%n", components);
+	// 	scc = new SCC(SCC2_GRAPH_PATH, GraphType.UNWEIGHTED);
+	// 	components = scc.execute();
+	// 	System.out.printf("Components of Graph 2 are: %s%n", components);
 
-		scc = new SCC(SCC3_GRAPH_PATH, GraphType.UNWEIGHTED);
-		components = scc.execute();
-		System.out.printf("Components of Graph 3 are: %s%n", components);
+	// 	scc = new SCC(SCC3_GRAPH_PATH, GraphType.UNWEIGHTED);
+	// 	components = scc.execute();
+	// 	System.out.printf("Components of Graph 3 are: %s%n", components);
 
-		scc = new SCC(SCC4_GRAPH_PATH, GraphType.UNWEIGHTED);
-		components = scc.execute();
-		System.out.printf("Components of Graph 4 are: %s%n", components);
+	// 	scc = new SCC(SCC4_GRAPH_PATH, GraphType.UNWEIGHTED);
+	// 	components = scc.execute();
+	// 	System.out.printf("Components of Graph 4 are: %s%n", components);
 
-	}
+	// }
 
 	public static void main(String args[]) throws Exception {
-		System.out.println("****************** Union Find ******************\n");
-		testUnionFind();
-		System.out.println("\n****************** Kruskal ******************\n");
-		testKruskal();
+		// System.out.println("****************** Union Find ******************\n");
+		// testUnionFind();
+		// System.out.println("\n****************** Kruskal ******************\n");
+		// testKruskal();
 		System.out.println("\n****************** KMP ******************\n");
 		testKMP();
-		System.out.println("****************** Strongly Connected Components ******************\n");
-		testSCC();
+		// System.out.println("****************** Strongly Connected Components ******************\n");
+		// testSCC();
 	}
 }
